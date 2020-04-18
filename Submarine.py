1050f8f5ba89fa671bfe2f2f0b7922a62a75f969
Gleb <gdenisov.hcmc@gmail.com>
2020-04-18 19:32:21 +0700

    Change date

--- Submarine.py
+++ Submarine.py
@@ -1,6 +1,6 @@
 
 ##Submarine Game
-## Created by Gleb Denisov on 10-Sep-2019
+## Created by Gleb Denisov on 18-Apr-2020
 
 # GUI of the Game
 from tkinter import *
@@ -12,12 +12,12 @@ c = Canvas(window, width=WIDTH, height=HEIGHT, bg='DARKBLUE')
 c.pack()
 ship_id = c.create_polygon(5, 5, 5, 25, 30, 15,fill='red')
 ship_id2 = c.create_oval(0, 0, 30, 30, outline='red')
-SHIP_R = 15
+SHIP_R = 10
 MID_X = WIDTH / 2
 MID_Y = HEIGHT / 2
 c.move(ship_id, MID_X, MID_Y)
 c.move(ship_id2, MID_X, MID_Y)
-SHIP_SPD = 10
+SHIP_SPD = 5
 def move_ship(event):
     if event.keysym == 'w':         # Up
          c.move(ship_id, 0, -SHIP_SPD)
