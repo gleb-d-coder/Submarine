f61e1f713da2ec68dee401091ff9da17a42b591a
Gleb <gdenisov.hcmc@gmail.com>
2020-04-19 19:01:03 +0700

    Fix some issues

--- Submarine.py
+++ Submarine.py
@@ -1,12 +1,12 @@
-#.______    __    __  .______   .______    __       _______      _______.    __    __   __    __  .__   __. .___________. _______ .______      
-#|   _  \  |  |  |  | |   _  \  |   _  \  |  |     |   ____|    /       |   |  |  |  | |  |  |  | |  \ |  | |           ||   ____||   _  \     
-#|  |_)  | |  |  |  | |  |_)  | |  |_)  | |  |     |  |__      |   (----`   |  |__|  | |  |  |  | |   \|  | `---|  |----`|  |__   |  |_)  |    
-#|   _  <  |  |  |  | |   _  <  |   _  <  |  |     |   __|      \   \       |   __   | |  |  |  | |  . `  |     |  |     |   __|  |      /     
-#|  |_)  | |  `--'  | |  |_)  | |  |_)  | |  `----.|  |____ .----)   |      |  |  |  | |  `--'  | |  |\   |     |  |     |  |____ |  |\  \----.
-#|______/   \______/  |______/  |______/  |_______||_______||_______/       |__|  |__|  \______/  |__| \__|     |__|     |_______|| _| `._____|
+# .______    __    __  .______   .______    __       _______      _______.    __    __   __    __  .__   __. .___________. _______ .______
+# |   _  \  |  |  |  | |   _  \  |   _  \  |  |     |   ____|    /       |   |  |  |  | |  |  |  | |  \ |  | |           ||   ____||   _  \
+# |  |_)  | |  |  |  | |  |_)  | |  |_)  | |  |     |  |__      |   (----`   |  |__|  | |  |  |  | |   \|  | `---|  |----`|  |__   |  |_)  |
+# |   _  <  |  |  |  | |   _  <  |   _  <  |  |     |   __|      \   \       |   __   | |  |  |  | |  . `  |     |  |     |   __|  |      /
+# |  |_)  | |  `--'  | |  |_)  | |  |_)  | |  `----.|  |____ .----)   |      |  |  |  | |  `--'  | |  |\   |     |  |     |  |____ |  |\  \----.
+# |______/   \______/  |______/  |______/  |_______||_______||_______/       |__|  |__|  \______/  |__| \__|     |__|     |_______|| _| `._____|
 ## Created by Gleb Denisov on 18-Apr-2020
 
-### GUI of the Game
+# GUI of the Game
 
 # Import section
 from tkinter import *
@@ -14,50 +14,51 @@ from random import randint
 from math import sqrt
 from time import sleep, time
 
-
-
-### 001 The game main window
+# 001 The game main window
 HEIGHT = 500
 WIDTH = 800
 window = Tk()
-window.title("Buble Blaster")
+window.title("Bubble Blaster")
 c = Canvas(window, width=WIDTH, height=HEIGHT, bg='DARKBLUE')
 c.pack()
 
-### 002 Draw submarine
-ship_id = c.create_polygon(5, 5, 5, 25, 30, 15,fill='red')
+# 002 Draw submarine
+ship_id = c.create_polygon(5, 5, 5, 25, 30, 15, fill='red')
 ship_id2 = c.create_oval(0, 0, 30, 30, outline='red')
-SHIP_R = 10
+SHIP_R = 15
 MID_X = WIDTH / 2
 MID_Y = HEIGHT / 2
 c.move(ship_id, MID_X, MID_Y)
 c.move(ship_id2, MID_X, MID_Y)
 
-### 003 Submarine manage
-SHIP_SPD = 5
+# 003 Submarine manage
+SHIP_SPD = 10
+
+
 def move_ship(event):
     # Up
-    if event.keysym == 'w':         
-         c.move(ship_id, 0, -SHIP_SPD)
-         c.move(ship_id2, 0, -SHIP_SPD)
+    if event.keysym == 'w':
+        c.move(ship_id, 0, -SHIP_SPD)
+        c.move(ship_id2, 0, -SHIP_SPD)
     # Down
-    elif event.keysym == 's':       
+    elif event.keysym == 's':
         c.move(ship_id, 0, SHIP_SPD)
         c.move(ship_id2, 0, SHIP_SPD)
     # Left
-    elif event.keysym == 'a':       
+    elif event.keysym == 'a':
         c.move(ship_id, -SHIP_SPD, 0)
         c.move(ship_id2, -SHIP_SPD, 0)
     # Right
-    elif event.keysym == 'd':       
+    elif event.keysym == 'd':
         c.move(ship_id, SHIP_SPD, 0)
         c.move(ship_id2, SHIP_SPD, 0)
+
+
 c.bind_all('<Key>', move_ship)
 
+# It's the bubble time!!!
 
-### It's the bubble time!!!
-
-### 004 Bubbles moving
+# 004 Bubbles moving
 bub_id = list()
 bub_r = list()
 bub_speed = list()
@@ -65,6 +66,7 @@ MIN_BUB_R = 10
 MAX_BUB_R = 30
 MAX_BUB_SPD = 10
 GAP = 100
+
 
 def create_buble():
     x = WIDTH + GAP
@@ -75,67 +77,77 @@ def create_buble():
     bub_r.append(r)
     bub_speed.append(randint(1, MAX_BUB_SPD))
 
-### 005 Move bubbles
+
+# 005 Move bubbles
 def move_bubbles():
     for i in range(len(bub_id)):
         c.move(bub_id[i], -bub_speed[i], 0)
-        
 
-### 007
+
+# 007
 def get_coords(id_num):
     pos = c.coords(id_num)
-    x = (pos[0] + pos[2])/2
-    y = (pos[1] + pos[3])/2
+    x = (pos[0] + pos[2]) / 2
+    y = (pos[1] + pos[3]) / 2
     return x, y
 
-### 008
-def del_bubbles(i):
+
+# 008
+def del_bubble(i):
     del bub_r[i]
     del bub_speed[i]
     c.delete(bub_id[i])
     del bub_id[i]
 
-### 009
+
+# 009
 def clean_up_bubs():
-    for i in range(len(bub_id)-1, -1, -1):
+    for i in range(len(bub_id) - 1, -1, -1):
         x, y = get_coords(bub_id[i])
         if x < -GAP:
             del_bubble(i)
 
-### 011
+
+# 011
 def distance(id1, id2):
     x1, y1 = get_coords(id1)
     x2, y2 = get_coords(id2)
-    return sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    return sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
-### 012
+
+# 012
 def collision():
     points = 0
-    for bub in range(len(bub_id)-1, -1, -1,):
+    for bub in range(len(bub_id) - 1, -1, -1, ):
         if distance(ship_id2, bub_id[bub]) < (SHIP_R + bub_r[bub]):
             points += (bub_r[bub] + bub_speed[bub])
             del_bubble(bub)
     return points
 
-### 014
-c.create_text(50, 30, text='TIME', fill='white' )
+
+# 014
+c.create_text(50, 30, text='TIME', fill='white')
 c.create_text(150, 30, text='SCORE', fill='white')
 time_text = c.create_text(50, 50, fill='white')
 score_text = c.create_text(150, 50, fill='white')
+
+
 def show_score(score):
     c.itemconfig(score_text, text=str(score))
+
+
 def show_time(time_left):
     c.itemconfig(time_text, text=str(time_left))
 
 
-### 015 INIT GAME PARAMETERS
+# 015 INIT GAME PARAMETERS
 BUB_CHANCE = 10
 TIME_LIMIT = 40
 BONUS_SCORE = 1000
 score = 0
 bonus = 0
 end = time() + TIME_LIMIT
-### 006/010/013/016 MAIN GAME LOOP
+# 006/010/013/016 MAIN GAME LOOP
 while time() < end:
     if randint(1, BUB_CHANCE) == 1:
         create_buble()
@@ -146,13 +158,9 @@ while time() < end:
         end += TIME_LIMIT
     show_score(score)
     show_time(int(end - time()))
-    #print(score)
     window.update()
     sleep(0.01)
 
-
-c.create_text(MID_X, MID_Y, text='GAME OVER', fill='white', font=('Helvetica',30))
-c.create_text(MID_X, MID_Y + 30, \
-    text+'Score: '+ str(score), fill='white')
-c.create_text(MID_X, MID_Y + 45, \
-    text+'Bonus time: '+ str(bonus*TIME_LIMIT), fill+'white') 
+c.create_text(MID_X, MID_Y, text='GAME OVER', fill='white', font=('Helvetica', 30))
+c.create_text(MID_X, MID_Y + 30, text='Score: ' + str(score), fill='white')
+c.create_text(MID_X, MID_Y + 45, text='Bonus time: ' + str(bonus * TIME_LIMIT), fill='white')
